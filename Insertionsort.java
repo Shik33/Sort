bfda84313d2c3c1d6dff1a4014e179f0f40b8e08
Shik33 <38437596+Shik33@users.noreply.github.com>
2018-08-05 18:34:40 +0530

    Update Insertionsort.java

--- Insertionsort.java
+++ Insertionsort.java
@@ -1,57 +1,47 @@
+/**
+ * Program for Insertion sort
+ * @author shikhir
+ *
+ */
 import java.util.Scanner;
+public class Insertion {
 
-public class Insertion_Sort {
-	
-	
-    void sort(int A[])
-    {
-        int n = A.length;
-        for (int i=1; i<n; ++i)
-        {
-            int key = A[i];
-            int j = i-1;
- 
-          
-            while (j>=0 && A[j] > key)
-            {
-                A[j+1] = A[j];
-                j = j-1;
-            }
-            A[j+1] = key;
-        }
-    }
- 
-   
-    static void printArray(int A[])
-    {
-        int n = A.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(A[i] + " ");
- 
-        System.out.println();
-    }
- 
-    
-    public static void main(String args[])
-    {  
-    	Scanner s=new Scanner(System.in);
-    	System.out.println("Enter the number of elements");
-    	int n=s.nextInt();
-    	int A[]=new int[n];
-    	System.out.println("Enter elements");
-    	for(int i=0;i<n;i++) {
-    		A[i]=s.nextInt();
-    	}
-    	for(int i: A) {
-    		System.out.println(i);
-    	}
-       
- 
-        Insertion_Sort ob = new Insertion_Sort();        
-        ob.sort(A);
-         
-        printArray(A);
-    }
-    
+	private static Scanner s;
+	void sort(int A[]) {
+		int n = A.length;
+		for (int i = 1; i < n; ++i) {
+			int key = A[i];
+			int j = i - 1;
+			while (j >= 0 && A[j] > key) {
+				A[j + 1] = A[j];
+				j = j - 1;
+			}
+			A[j + 1] = key;
+		}
+	}
+	static void printArray(int A[]) {
+		int n = A.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(A[i] + " ");
+		System.out.println();
+	}
+	public static void main(String args[]) {
+		s = new Scanner(System.in);
+		System.out.println("Enter the number of elements");
+		int n = s.nextInt();
+		int A[] = new int[n];
+		System.out.println("Enter elements");
+		for (int i = 0; i < n; i++) {
+			A[i] = s.nextInt();
+		}
+		for (int i : A) {
+			System.out.println(i);
+		}
+
+		Insertion ob = new Insertion();
+		ob.sort(A);
+
+		printArray(A);
+	}
 
 }
